6c56c647f6db62931d738fbe5734f15c5edb7a9d
chanKausa <kausaaws@gmail.com>
2024-03-02 12:42:53 +0530

    eslint imp.

--- api/index.js
+++ api/index.js
@@ -1,2 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
 const app = express();
+const envConfig = process.env;
+
+(async () => {
+  await mongoose.connect(envConfig.MONGO_URL);
+})();
+
+require('./model');
